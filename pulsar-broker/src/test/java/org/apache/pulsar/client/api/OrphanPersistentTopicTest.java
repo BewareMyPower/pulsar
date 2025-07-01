2c0681fc4eb06963f70a7f4a7a9df6aa9ee5a6f9
Yunze Xu <xyzinfernity@163.com>
2025-07-02 01:52:33 +0800

    Reduce test time of OrphanPersistentTopicTest

--- pulsar-broker/src/test/java/org/apache/pulsar/client/api/OrphanPersistentTopicTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/client/api/OrphanPersistentTopicTest.java
@@ -84,13 +84,7 @@ public class OrphanPersistentTopicTest extends ProducerConsumerBase {
         // Make topic load timeout 5 times.
         AtomicInteger timeoutCounter = new AtomicInteger();
         for (int i = 0; i < 5; i++) {
-            mockZooKeeper.delay(topicLoadTimeoutSeconds * 2 * 1000, (op, path) -> {
-                if (mlPath.equals(path)) {
-                    log.info("Topic load timeout: " + timeoutCounter.incrementAndGet());
-                    return true;
-                }
-                return false;
-            });
+            delayMetadataOperation(i, tpName);
         }
 
         // Load topic.
@@ -130,15 +124,7 @@ public class OrphanPersistentTopicTest extends ProducerConsumerBase {
         String tpName = BrokerTestUtil.newUniqueName("persistent://public/default/tp2");
 
         // Mock message deduplication recovery speed topicLoadTimeoutSeconds
-        String mlPath = BrokerService.MANAGED_LEDGER_PATH_ZNODE + "/" +
-                TopicName.get(tpName).getPersistenceNamingEncoding() + "/" + DEDUPLICATION_CURSOR_NAME;
-        mockZooKeeper.delay(topicLoadTimeoutSeconds * 1000, (op, path) -> {
-            if (mlPath.equals(path)) {
-                log.info("Topic load timeout: " + path);
-                return true;
-            }
-            return false;
-        });
+        delayMetadataOperation(0, tpName);
 
         // First load topic will trigger timeout
         // The first topic load will trigger a timeout. When the topic closes, it will call transactionBuffer.close.
@@ -312,5 +298,17 @@ public class OrphanPersistentTopicTest extends ProducerConsumerBase {
         }
         consumer.close();
         admin.topics().delete(tpName);
+    }
+
+    private void delayMetadataOperation(int i, String topic) {
+        final var mlPath = BrokerService.MANAGED_LEDGER_PATH_ZNODE + "/"
+                + TopicName.get(topic).getPersistenceNamingEncoding() + "/" + DEDUPLICATION_CURSOR_NAME;
+        mockZooKeeper.delay(conf.getTopicLoadTimeoutSeconds() * 1000 + 500, (__, path) -> {
+            if (mlPath.equals(path)) {
+                log.info("Topic {} load timeout: {}", i, path);
+                return true;
+            }
+            return false;
+        });
     }
 }
